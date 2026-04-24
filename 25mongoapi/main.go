02721ebcbc43901225e3ce937058f9d5bcf25035
AbhayMishra2722 <abhaymishra2722@gmail.com>
2026-04-25 00:10:42 +0530

    MongoAPI

--- 25mongoapi/main.go
+++ 25mongoapi/main.go
@@ -12,7 +12,7 @@ func main() {
 	fmt.Println("MongoDB API")
 	r := router.Router()
 	fmt.Println("Server is getting started...")
-	log.Fatal(http.ListenAndServe(":4000", r))
-	fmt.Println("Listening at port 4000...")
+	log.Fatal(http.ListenAndServe(":5000", r))
+	fmt.Println("Listening at port 5000...")
 
 }
